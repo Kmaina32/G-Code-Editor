64a033173db13f58f12df632c8ec424c4ac1d0d5
Ubuntu Academy <kishansan28@gmail.com>
2025-09-16 08:54:33 +0000

    client-side code execution:
    For Python: We can use a library called Pyo

--- public/js-worker.js
+++ public/js-worker.js
@@ -1,66 +1,31 @@
-// A simple web worker to execute javascript code in a sandboxed environment.
-// It hijacks console.log and console.error to post messages back to the main thread.
+// Simple web worker to execute user's javascript code
+// It hijacks console.log and console.error to post messages back to the main thread
 
 self.onmessage = (event) => {
-  const { code } = event.data;
-  const logs = [];
+    const { code } = event.data;
 
-  // Store original console methods
-  const originalLog = console.log;
-  const originalError = console.error;
-  const originalWarn = console.warn;
-  const originalInfo = console.info;
-  const originalClear = console.clear;
+    // Hijack console.log
+    const originalLog = console.log;
+    console.log = (...args) => {
+        self.postMessage({ type: 'log', message: args.join(' ') });
+        originalLog.apply(console, args);
+    };
 
-  // Hijack console methods
-  console.log = (...args) => {
-    const message = args.map(arg => formatArg(arg)).join(' ');
-    self.postMessage({ type: 'log', message });
-    originalLog.apply(console, args);
-  };
-  console.error = (...args) => {
-    const message = args.map(arg => formatArg(arg)).join(' ');
-    self.postMessage({ type: 'error', message });
-    originalError.apply(console, args);
-  };
-   console.warn = (...args) => {
-    const message = args.map(arg => formatArg(arg)).join(' ');
-    self.postMessage({ type: 'warn', message });
-    originalWarn.apply(console, args);
-  };
-   console.info = (...args) => {
-    const message = args.map(arg => formatArg(arg)).join(' ');
-    self.postMessage({ type: 'info', message });
-    originalInfo.apply(console, args);
-  };
-   console.clear = () => {
-    self.postMessage({ type: 'clear' });
-    originalClear.apply(console);
-  };
+    // Hijack console.error
+    const originalError = console.error;
+    console.error = (...args) => {
+        self.postMessage({ type: 'error', message: `ERROR: ${args.join(' ')}` });
+        originalError.apply(console, args);
+    };
 
+    try {
+        // Execute the user's code
+        eval(code);
+    } catch (e) {
+        console.error(e);
+    }
 
-  try {
-    // Use Function constructor for safer execution than eval
-    new Function(code)();
-  } catch (e) {
-    console.error(e);
-  } finally {
-     // Restore original console methods
+    // Restore original console functions
     console.log = originalLog;
     console.error = originalError;
-    console.warn = originalWarn;
-    console.info = originalInfo;
-    console.clear = originalClear;
-  }
 };
-
-function formatArg(arg) {
-    if (typeof arg === 'object' && arg !== null) {
-        try {
-            return JSON.stringify(arg);
-        } catch (e) {
-            return '[Circular Object]';
-        }
-    }
-    return String(arg);
-}
